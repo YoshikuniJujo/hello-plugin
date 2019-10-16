85ff540fe9386d8f312b8a5a26d2c8d290693499
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2019-10-17 08:59:57 +0900

    comment out: -dcore-lint

--- src/Foo/Test.hs
+++ src/Foo/Test.hs
@@ -11,24 +11,24 @@ class Foo a
 foo :: Foo a => a -> a
 foo = id
 
-three :: Int
-three = foo 3
+-- three :: Int
+-- three = foo 3
 
 class Bar (n :: Nat)
 
 bar :: Bar n => Proxy n -> Proxy n
 bar = id
 
-eight :: Proxy 8
-eight = bar Proxy
+-- eight :: Proxy 8
+-- eight = bar Proxy
 
 class Baz (s :: Symbol)
 
 baz :: Baz s => Proxy s -> Proxy s
 baz = id
 
-hello :: Proxy "hello"
-hello = baz Proxy
+-- hello :: Proxy "hello"
+-- hello = baz Proxy
 
 -- world :: Proxy "world"
 -- world = baz Proxy
